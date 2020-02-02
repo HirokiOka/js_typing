d882e97dd404f87a7c3d3aa769978d5b991addda
hiroki <hiro_bass.0827@icloud.com>
2020-02-02 13:10:39 +0900

    fix ACCESS_TOKEN

--- src/utils/gist.js
+++ src/utils/gist.js
@@ -3,9 +3,7 @@ const path = require('path');
 const ENV_PATH = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_PATH });
 
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-
-//GitHub APIにアクセスしてJavaScriptのgistをとってくる関数
+// const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 const getJS = (callback) => {
     const index = Math.floor(Math.random() * 30);
