c79ac5b9bfa69fb23c31e3771c8bb5293ba847e9
hiroki <hiro_bass.0827@icloud.com>
2021-09-15 10:39:25 +0900

    fix package-lock.json

---
+++ public/js/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Main() {
+  return(
+    <h1>Hello </h1>
+    );
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
+
